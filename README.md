c714b713c3813c38ef6ff89c6dae0c95846a91f7
ctuu <ctuu1s@outlook.com>
2018-09-27 17:03:14 +0800

    initial.

---
+++ README.md
@@ -0,0 +1,43 @@
+ACM-ICPC Code Template
+==============================
+Generate ACM-ICPC Code Template with LaTex.
+
+(1) Write code.
+
+(2) Place your code in **./source** and classify in different subdirectory.
+
+Example:
+```
+.
+└───source
+    ├── DataStructure
+    │   └── Code_1.cpp
+    ├── Graph
+    │   └── Code_1.cpp
+    └── Math
+        ├── Code_1.cpp
+        └── Code_2.cpp
+```
+## Required
+- LaTex
+- Python
+- (optional) Clang-Format
+
+## Usage
+``` python
+python gen.py
+```
+## feature
+- Support CJK
+- Can format code with [Clang-Format](http://clang.llvm.org/docs/ClangFormat.html)
+
+Switch True/False the *Format function* by change the `FORMAT` value in **./gen.py** (default False)
+
+Sorted by lexicographical order.
+
+**./head:** you can change the author and font here.</br>
+**./.clang-format:** set the code style if necessary.</br>
+
+Code inside is for fun, that's all.
+
+The `README.md` & `head` are based on [ACM-ICPC-CodeTemplate-Latex @jasison27](https://github.com/jasison27/ACM-ICPC-CodeTemplate-Latex)
---
+++ gen.py
@@ -0,0 +1,92 @@
+import os
+IGNORE = ['config', '.gitkeep']
+HEAD = 'head'
+TAIL = 'tail'
+PATH = 'source'
+FILENAME = 'template'
+FORMAT = False
+
+_list = []
+_dict = {}
+_root = os.path.expanduser(PATH)
+
+
+def _format(path):
+    os.system('clang-format -i "%s"' % (path))
+
+
+def p_add(path, name, parent, suffix):
+    name = name.replace('_', '\\_')
+    if FORMAT is True:
+        _format(path)
+    path_tuple = (path, name, suffix)
+    if parent not in _dict:
+        _dict[parent] = []
+    _dict[parent].append(path_tuple)
+
+
+def p_walk(root, parent):
+    if not os.path.isdir(root):
+        os.mkdir(root)
+        return
+    for i in os.listdir(root):
+        if i in IGNORE:
+            continue
+        cur_path = os.path.join(root, i)
+
+        if os.path.isdir(cur_path):
+            p_walk(cur_path, i)
+            continue
+
+        name, suffix = os.path.splitext(i)
+        p_add(cur_path, name, parent, suffix)
+
+
+def p_tran():
+    for c in _dict:
+        _tuple = (c, _dict[c])
+        _list.append(_tuple)
+
+
+def p_sort():
+    _list.sort(key=lambda c: c[0])
+    for _sub in _list:
+        _sub[1].sort(key=lambda c: c[2])
+        _sub[1].sort(key=lambda c: c[1])
+
+
+def p_gen():
+    _doc = open(FILENAME + '.tex', 'w', encoding='utf-8')
+
+    with open(HEAD, 'r', encoding='utf-8') as r:
+        _doc.write(r.read())
+
+    for section, sub_list in _list:
+        _doc.write('\\section{%s}\n' % (section))
+        for path, name, suffix in sub_list:
+            _doc.write('\\subsection{%s}\n' % (name))
+            _doc.write('\\lstinputlisting{%s}\n' % (path))
+            suffix = suffix
+
+    with open(TAIL, 'r', encoding='utf-8') as r:
+        _doc.write(r.read())
+
+    _doc.close()
+
+
+def p_build():
+    os.system('xelatex %s.tex' % FILENAME)
+    os.unlink('%s.pdf' % FILENAME)
+    os.system('xelatex %s.tex' % FILENAME)
+    os.unlink('%s.aux' % FILENAME)
+    os.unlink('%s.log' % FILENAME)
+    os.unlink('%s.toc' % FILENAME)
+
+
+p_walk(_root, 'Not classified')
+p_tran()
+p_sort()
+p_gen()
+p_build()
+
+print('Done.')
